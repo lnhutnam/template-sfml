81036d66357f84cd1b530d4fd9c9a9d955b32cba
nhutnamhcmus <lenam.fithcmus@gmail.com>
2021-01-12 12:48:37 +0700

    push template

---
+++ cmake-build-debug/CMakeFiles/template_sfml.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template_sfml.dir/main.cpp.obj"
+  "libtemplate_sfml.dll.a"
+  "template_sfml.exe"
+  "template_sfml.exe.manifest"
+  "template_sfml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/template_sfml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/test_sfml.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_sfml.dir/main.cpp.obj"
+  "libtest_sfml.dll.a"
+  "test_sfml.exe"
+  "test_sfml.exe.manifest"
+  "test_sfml.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_sfml.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
